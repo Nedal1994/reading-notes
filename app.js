a5920b963383af6300136fdd815d2a8590bfb567
Nedal1994 <85867422+Nedal1994@users.noreply.github.com>
2021-06-16 14:46:02 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+var favfruit = prompt("What is your favorite fruit ?(apple, pomegranate, orange)?")					  
+if(favfruit == "apple")
+{document.write("<p>"+ "It is available" +"</p>")}			 				  
+else if(favfruit == "pomegranate"){document.write("<p>"+ "It is available"+"</p>")} 
+else if(favfruit == "orange"){document.write("<p>"+ "It is available" +"</p>")}		  
+else 
+{ alert("Not available") }
